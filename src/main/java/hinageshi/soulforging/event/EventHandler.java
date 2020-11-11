93a5e269d1931c7e5e9124f7b953fbe092f2d550
Hinageshi <2247001665@qq.com>
2020-11-11 22:40:14 +0800

    为常见实体的战利品表中添加mod掉落物

--- src/main/java/hinageshi/soulforging/event/EventHandler.java
+++ src/main/java/hinageshi/soulforging/event/EventHandler.java
@@ -23,22 +23,65 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import java.util.ArrayList;
 
 import static hinageshi.soulforging.item.ItemRegistryHandler.BROKEN_SOUL;
+import static hinageshi.soulforging.item.ItemRegistryHandler.INNOCENT_SOUL;
 
 @EventBusSubscriber
 public class EventHandler {
+    private static void addItemToEntityTable(LootTableLoadEvent event,LootTable loot,String entityName,int isMob){
+        if(event.getName().getResourcePath().equals("entities/" + entityName)) {
+            if(isMob == 1){
+                LootUtils.addItemToTable(loot, BROKEN_SOUL, 1, 1, 1, 1, 3, 0, 1, "soulforging:broken_soul",
+                        new LootUtils.IMethod() {
+                            @Override
+                            public void FunctionsCallback(ArrayList<LootFunction> lootfuncs) {
+                                LootFunction looting= new LootingEnchantBonus(null, new RandomValueRange(0,1),0);
+                                lootfuncs.add(looting);
+                            }
+                        }
+                );
+            }
+            else {
+                LootUtils.addItemToTable(loot, INNOCENT_SOUL, 1, 1, 1, 1, 3, 0, 1, "soulforging:innocent_soul",
+                        new LootUtils.IMethod() {
+                            @Override
+                            public void FunctionsCallback(ArrayList<LootFunction> lootfuncs) {
+                                LootFunction looting= new LootingEnchantBonus(null, new RandomValueRange(1,3),0);
+                                lootfuncs.add(looting);
+                            }
+                        }
+                );
+            }
+        }
+    }
+
     @SubscribeEvent
     public static void onLootLoad(LootTableLoadEvent event){
         LootTable loot = event.getTable();
-        if(event.getName().getResourcePath().equals("entities/zombie")) {
-            LootUtils.addItemToTable(loot, BROKEN_SOUL, 1, 1, 1, 2, 4, 0, 1, "soulforging:broken_soul",
-                new LootUtils.IMethod() {
-                    @Override
-                    public void FunctionsCallback(ArrayList<LootFunction> lootfuncs) { LootFunction looting = new LootingEnchantBonus(null, new RandomValueRange(0,1), 0);
-                        lootfuncs.add(looting);
-                    }
-                }
-            );
-        }
+        addItemToEntityTable(event,loot,"blaze",1);
+        addItemToEntityTable(event,loot,"chicken",0);
+        addItemToEntityTable(event,loot,"cow",0);
+        addItemToEntityTable(event,loot,"creeper",1);
+        addItemToEntityTable(event,loot,"enderman",1);
+        addItemToEntityTable(event,loot,"ghast",1);
+        addItemToEntityTable(event,loot,"iron_golem",0);
+        addItemToEntityTable(event,loot,"pig",0);
+        addItemToEntityTable(event,loot,"sheep",0);
+        addItemToEntityTable(event,loot,"silverfish",1);
+        addItemToEntityTable(event,loot,"skeleton",1);
+        addItemToEntityTable(event,loot,"slime",1);
+        addItemToEntityTable(event,loot,"snowman",0);
+        addItemToEntityTable(event,loot,"spider",1);
+        addItemToEntityTable(event,loot,"squid",0);
+        addItemToEntityTable(event,loot,"villager",0);
+        addItemToEntityTable(event,loot,"witch",1);
+        addItemToEntityTable(event,loot,"wolf",0);
+        addItemToEntityTable(event,loot,"zombie",1);
+
+        if(event.getName().getResourcePath().equals("entities/elder_guardian"))
+            LootUtils.addItemToTable(loot, BROKEN_SOUL, 1, 1, 1, 1, 3, 0, 1, "soulforging:broken_soul");
+
+        if(event.getName().getResourcePath().equals("entities/ender_dragon"))
+            LootUtils.addItemToTable(loot, BROKEN_SOUL, 1, 1, 1, 1, 3, 0, 1, "soulforging:broken_soul");
     }
 
     @SubscribeEvent
