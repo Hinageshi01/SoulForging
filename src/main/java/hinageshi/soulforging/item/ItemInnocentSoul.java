01181c9d3c4e9d136ea7174dce49b96466eda29b
Hinageshi <2247001665@qq.com>
2021-01-17 02:12:59 +0800

    无辜的灵魂现在可以食用并带来一些效果

--- src/main/java/hinageshi/soulforging/item/ItemInnocentSoul.java
+++ src/main/java/hinageshi/soulforging/item/ItemInnocentSoul.java
@@ -2,13 +2,32 @@ package hinageshi.soulforging.item;
 
 import hinageshi.soulforging.SoulForging;
 import hinageshi.soulforging.creativetab.TabSoulForging;
-import net.minecraft.item.Item;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
-public class ItemInnocentSoul extends Item {
+public class ItemInnocentSoul extends ItemFood {
     public ItemInnocentSoul(){
+        super(10,0.8F,false);
+        this.setPotionEffect(new PotionEffect(MobEffects.WITHER,180,2), 0.1F);
+        this.setAlwaysEdible();
         this.setUnlocalizedName(SoulForging.MODID + ".innocentSoul");
         this.setCreativeTab(TabSoulForging.TAB_SOUL_FORGING);
         this.setRegistryName("innocent_soul");
         this.setMaxStackSize(64);
+    }
+
+    @Override
+    public void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player)
+    {
+        if (!worldIn.isRemote)
+        {
+            player.addPotionEffect(new PotionEffect(MobEffects.NAUSEA, 120, 1));
+            player.addExperience(20);
+        }
+        super.onFoodEaten(stack, worldIn, player);
     }
 }
