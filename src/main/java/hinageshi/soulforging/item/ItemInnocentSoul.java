823d328f72091ce23b6dd493fe0336cf4ce8ab3a
Hinageshi <2247001665@qq.com>
2021-01-17 21:29:49 +0800

    禁止在满饱食度的时候食用无辜的灵魂，下调凋零的持续时间

--- src/main/java/hinageshi/soulforging/item/ItemInnocentSoul.java
+++ src/main/java/hinageshi/soulforging/item/ItemInnocentSoul.java
@@ -10,10 +10,10 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
 public class ItemInnocentSoul extends ItemFood {
-    public ItemInnocentSoul(){
+    public ItemInnocentSoul() {
         super(8,0.8F,false);
-        this.setPotionEffect(new PotionEffect(MobEffects.WITHER,180,2), 0.1F);
-        //this.setAlwaysEdible();
+        this.setPotionEffect(new PotionEffect(MobEffects.WITHER,160,2), 1F);
+        this.setAlwaysEdible();
         this.setUnlocalizedName(SoulForging.MODID + ".innocentSoul");
         this.setCreativeTab(TabSoulForging.TAB_SOUL_FORGING);
         this.setRegistryName("innocent_soul");
@@ -21,10 +21,8 @@ public class ItemInnocentSoul extends ItemFood {
     }
 
     @Override
-    public void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player)
-    {
-        if (!worldIn.isRemote)
-        {
+    public void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
+        if (!worldIn.isRemote) {
             player.addPotionEffect(new PotionEffect(MobEffects.NAUSEA, 110, 1));
             player.addExperience(20);
         }
