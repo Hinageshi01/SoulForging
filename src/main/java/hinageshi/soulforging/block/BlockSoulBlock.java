c0172d41d758217ce3500657a8f4b911cb69767b
Hinageshi <2247001665@qq.com>
2021-01-18 17:39:00 +0800

    更改灵魂块材质属性并提高其挖掘等级

--- src/main/java/hinageshi/soulforging/block/BlockSoulBlock.java
+++ src/main/java/hinageshi/soulforging/block/BlockSoulBlock.java
@@ -7,11 +7,11 @@ import net.minecraft.block.material.Material;
 
 public class BlockSoulBlock extends Block {
     public BlockSoulBlock(){
-        super(Material.GROUND);
+        super(Material.ROCK);
         this.setUnlocalizedName(SoulForging.MODID + ".soulBlock");
         this.setCreativeTab(TabSoulForging.TAB_SOUL_FORGING);
         this.setRegistryName("soul_block");
-        this.setHarvestLevel("shovel",0);
+        this.setHarvestLevel("pickaxe",1);
         this.setHardness(0.5F);
         this.setLightLevel(1F);
     }
